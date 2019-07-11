0b7bc8eb6035e51523ebf1b2e433d17fc0825977
Marcin Guzek <ravenwroc@interia.pl>
2019-07-11 15:47:09 +0200

    Updates

---
+++ precache-manifest.113cf984efa0a7b1c1facfb00f9b88ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2345e8f42af0cd6c512a2836762e5f46",
+    "url": "/oddaj-rzeczy/index.html"
+  },
+  {
+    "revision": "e0e3bd711f69e15863ee",
+    "url": "/oddaj-rzeczy/static/css/main.3619771c.chunk.css"
+  },
+  {
+    "revision": "01ba71c44f777a7a7352",
+    "url": "/oddaj-rzeczy/static/js/2.21eb3eda.chunk.js"
+  },
+  {
+    "revision": "e0e3bd711f69e15863ee",
+    "url": "/oddaj-rzeczy/static/js/main.5ae3eb37.chunk.js"
+  },
+  {
+    "revision": "6a47cef5096a86694c41",
+    "url": "/oddaj-rzeczy/static/js/runtime~main.87ab987b.js"
+  },
+  {
+    "revision": "2c60c6fe9605a0b26170b834fdc6244f",
+    "url": "/oddaj-rzeczy/static/media/header_start_img.2c60c6fe.jpg"
+  },
+  {
+    "revision": "eb0e1c044b90f1a349f8754b939e577f",
+    "url": "/oddaj-rzeczy/static/media/statisticsBackground.eb0e1c04.jpg"
+  }
+]);
